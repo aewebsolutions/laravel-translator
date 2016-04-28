d08d157275dcab6e4c2a24376026165fe0cb2746
aewebsolutions <info@aesolucionesweb.com.ar>
2016-04-28 11:55:41 -0300

    Update TranslatorURLInterface.php
    Complete all methods

--- src/Contracts/TranslatorURLInterface.php
+++ src/Contracts/TranslatorURLInterface.php
@@ -6,5 +6,13 @@ interface TranslatorURLInterface
 {
     public function route($name, $parameters = [], $absolute = true, $locale = NULL);
     
+    public function routes($name, $parameters = [], $absolute = true);
+    
     public function current($locale = NULL, $absolute = true);
+    
+    public function hasRouteLocale($route, $locale);
+    
+    public function hasLocale($routename, $locale);
+    
+    public function localize($uri, $locale, $absolute = true, $uriHasPrefix = true);
 }
